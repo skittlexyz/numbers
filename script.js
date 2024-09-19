6c6f99f9b205d8d2eeb906edecbd34e4193030ee
Moisés Corrêa Gomes <54242235+skittlexyz@users.noreply.github.com>
2024-09-19 17:28:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const rowsInput = document.getElementById("rows");
 const columnsInput = document.getElementById("columns");
 const sizeInput = document.getElementById("size");
+const decimalsInput = document.getElementById("decimals");
 const minInput = document.getElementById("min");
 const maxInput = document.getElementById("max");
 const bgColorInput = document.getElementById("bg-color");
 const textColorInput = document.getElementById("text-color");
+const delayInput = document.getElementById("delay");
 
 sizeInput.addEventListener("change", () => {
     document.querySelectorAll(".number").forEach((element) => {
@@ -36,12 +38,41 @@ document.querySelector("#close-menu").addEventListener("click", () => {
     document.querySelector("#open-menu").classList.remove("hide");
 });
 
+delayInput.addEventListener("input", () => {
+    const interval_id = window.setInterval(function () { }, Number.MAX_SAFE_INTEGER);
+    for (let i = 1; i < interval_id; i++) { window.clearInterval(i); }
+    start();
+})
+
 const rowElement = document.createElement("div");
 rowElement.className = "flex justify-content items-center space-between"
+
+let numbers = [1];
 
 const numberElement = document.createElement("span");
 numberElement.className = "number font-3xl"
 
 rowElement.append(numberElement);
+rowElement.append(numberElement);
 document.querySelector("main").append(rowElement);
 numberElement.innerText = "13.43269"
+
+function start() {
+    if (numbers.length != 0) setInterval(() => {
+        document.querySelectorAll(".number").forEach((element) => {
+            numbers.push(element);
+        })
+        for (let i = 0; i < numbers.length; i++) {
+            const factor = Math.pow(10, decimalsInput.value);
+            let randomFloatString = (Math.floor((Math.random() * (maxInput.value - minInput.value) + minInput.value) * factor) / factor + parseInt(minInput.value)).toFixed(decimalsInput.value).toString().split(".");
+            numbers[i].innerText = randomFloatString[0].padStart(2, "0") + "." + randomFloatString[1];
+        }
+        numbers = [1];
+    }, parseInt(delayInput.value));
+    else {
+        const interval_id = window.setInterval(function () { }, Number.MAX_SAFE_INTEGER);
+        for (let i = 1; i < interval_id; i++) { window.clearInterval(i); }
+    }
+}
+
+start();
